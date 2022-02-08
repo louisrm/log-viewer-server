eadb218beff3713f8d263b21b68206514c2cfb56
Louis Mueller <87721607+louisrm@users.noreply.github.com>
2022-02-07 22:20:08 -0600

    adding custom  post

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = {'.tlog'}
 
-# Members API route
-@app.route("/members")
-def members():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Example Data API route
+@app.route("/exampleData")
+def example_data():
     flightObjFile = open("flight_obj.json","r")
     flightObj = json.load(flightObjFile)
     response = app.response_class(
@@ -15,6 +20,30 @@ def members():
         mimetype='application/json'
     )
     return response
+
+@app.route("/customData", methods=['POST'])
+def custom_data():
+    print(request.get_data())
+
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        print('no file')
+        return 'nope', 400
+    file = request.files['file']
+  
+    if file and allowed_file(file.filename):
+        flightObjFile = open("flight_obj.json","r")
+        flightObj = json.load(flightObjFile)
+        response = app.response_class(
+            response=json.dumps(flightObj),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+
+    return 'extra nope', 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
